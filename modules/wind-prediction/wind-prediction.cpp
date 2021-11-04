3a84351dae3a52822a63b8446556c104998c0c6a
jf99 <jf99@users.noreply.github.com>
2021-11-04 20:36:36 +0100

    make wind prediction use the secret weather formula

--- modules/wind-prediction/wind-prediction.cpp
+++ modules/wind-prediction/wind-prediction.cpp
@@ -1,7 +1,8 @@
 #include "wind-prediction.h"
+#include "modules/weather-formula/weather-formula.h"
 
 void predictWind(float& x, float& y)
 {
-  x = 5.f;
-  y = -2.f;
+  x = 5.f * magicValue();
+  y = -2.f * magicValue();
 }
--- wind-prediction-demo.pro
+++ wind-prediction-demo.pro
@@ -7,3 +7,6 @@ SOURCES += \
         demo.cpp
 
 include(modules/wind-prediction/wind-prediction.pri)
+
+SUBMODULE_DIR=$$PWD/submodules
+include($$SUBMODULE_DIR/weather-formula/modules/weather-formula/weather-formula.pri)
